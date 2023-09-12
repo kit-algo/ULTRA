05f7a3c6b029adc586817ea906539049bd8e714f
ishika gupta <ishikaguptajmu@gmail.com>
2023-09-12 17:37:17 +0530

    Added question 1 and question 2

---
+++ Runnables/question1.cpp
@@ -0,0 +1,66 @@
+// Submission by Ishika Gupta
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <Dijkstra.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+struct Edge {
+    int u, v, w;
+    Edge(int u, int v, int w)
+    {
+        u=this.u;
+        v=this.v;
+        w=this.w;
+    } 
+};
+
+int main() {
+
+    ifstream file("USA-road-d.FLA.gr");
+
+    if (!file.is_open()) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    string line;
+    int numNodes, numEdges;
+    vector<Edge> edges;
+
+    while (getline(file, line)) 
+    {
+        if (line.empty() || line[0] == 'c') {
+            continue;
+        } else if (line[0] == 'p') {
+            char type[3];
+            sscanf(line.c_str(), "%2s %d %d", type, &numNodes, &numEdges);
+        } else if (line[0] == 'a') {
+            int u, v, w;
+            sscanf(line.c_str(), "a %d %d %d", &u, &v, &w);
+            edges.emplace_back(u, v, w);
+        }
+    }
+
+
+    for (int i = 0; i < 200; ++i) {
+        int source = rand() % numNodes + 1; 
+        int destination = rand() % numNodes + 1;
+        clock_t startTime = clock();
+
+        dijkstra.run(source, destination);
+
+        int shortestPathLength = dijkstra.getDistance(destination);
+        vector<Vertex> shortestPath = dijkstra.getPath(destination);
+
+        double elapsedTime = static_cast<double>(clock() - startTime) / CLOCKS_PER_SEC;
+        totalRuntime += elapsedTime;
+    }
+
+    cout << "Total runtime in seconds for 200 random Dijkstra: " << totalRuntime << endl;
+
+    return 0;
+}
